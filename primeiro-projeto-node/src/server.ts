a644d8f1015877fbd9308914b3472ca2e342ebf1
Guilherme Peixoto <guilherme@agily.com.br>
2020-04-21 02:00:18 -0300

    add eslint file configuration / add tolls and plugins eslint with airbnb

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: "Hello World!!!" }))
+app.get('/', (request, response) => response.json({ message: 'Hello World!!!' }));
 
-app.listen(3333, () => console.log(">> Server started on port 3333!"));
+app.listen(3333, () => console.log('>> Server started on port 3333!'));
