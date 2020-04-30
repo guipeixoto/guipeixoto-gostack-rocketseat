b75326188270d64e0249107afcd778cd4e80d103
Guilherme Peixoto <guilherme@agily.com.br>
2020-04-30 19:08:00 -0300

    inclusão da rota de sessions

---
+++ primeiro-projeto-node/src/services/AuthenticateUserService.ts
@@ -0,0 +1,45 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface RequestDTO {
+  email: string;
+  password: string;
+}
+
+interface ResponseDTO {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async run({ email, password }: RequestDTO): Promise<ResponseDTO> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const token = sign({}, '137286780b2afb7abe5195bed082ef58', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
