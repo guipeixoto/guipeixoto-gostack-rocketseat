6fc481088bd4e7f7b2d3fbf1a8167b1f045381e7
Guilherme Peixoto <guilherme@agily.com.br>
2020-04-29 23:48:48 -0300

    configuracao de relacionamento na model Appointment

--- primeiro-projeto-node/src/models/Appointment.ts
+++ primeiro-projeto-node/src/models/Appointment.ts
@@ -4,7 +4,10 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -22,6 +25,11 @@ class Appointment {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  // Relationships
+  @ManyToOne(() => User) // indica a cardinalidade do relacionamento
+  @JoinColumn({ name: 'provider_id' }) // informa qual coluna identfica o relacionamento
+  provider: User;
 }
 
 export default Appointment;
