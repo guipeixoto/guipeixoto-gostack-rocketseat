6915276f67211cd1af6a13911c98607f120366bc
Guilherme Peixoto <guilherme@agily.com.br>
2020-04-30 19:45:04 -0300

    pegando informações do arquivo de configuração

--- primeiro-projeto-node/src/services/AuthenticateUserService.ts
+++ primeiro-projeto-node/src/services/AuthenticateUserService.ts
@@ -3,6 +3,7 @@ import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
+import authConfig from '../config/auth';
 
 interface RequestDTO {
   email: string;
@@ -30,9 +31,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, '137286780b2afb7abe5195bed082ef58', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
