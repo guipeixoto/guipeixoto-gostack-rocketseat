707b2b5866771f86a2fa2cc0472a754022c278ef
Guilherme Peixoto <guilherme@agily.com.br>
2020-05-01 18:55:13 -0300

    navegando entre páginas

--- primeiro-projeto-react/src/pages/Repository/index.tsx
+++ primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParamsDTO {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository!</h1>;
+  const { params } = useRouteMatch<RepositoryParamsDTO>();
+
+  return <h1>Repository: {params.repository}</h1>;
 };
 
 export default Repository;
