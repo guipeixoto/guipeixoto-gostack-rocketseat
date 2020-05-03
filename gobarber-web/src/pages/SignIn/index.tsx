1afb50a0544ef3513e35a034437602556d9a9c10
Guilherme Peixoto <guilherme@agily.com.br>
2020-05-03 17:38:09 -0300

    isolando components HTML para reaproveitar

--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import { Container, Content, Background } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -13,11 +15,15 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input placeholder='E-mail' />
+        <Input name='email' placeholder='E-mail' icon={FiMail} />
+        <Input
+          name='password'
+          type='password'
+          placeholder='Senha'
+          icon={FiLock}
+        />
 
-        <input type='password' placeholder='Senha' />
-
-        <button type='submit'>Entrar</button>
+        <Button type='submit'>Entrar</Button>
 
         <a href='/forgot'>Esqueci minha senha</a>
       </form>
