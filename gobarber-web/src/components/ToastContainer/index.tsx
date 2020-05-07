da39dcaa8a309a974a1faa2495baffba39ef6b11
Guilherme Peixoto <guilherme@agily.com.br>
2020-05-07 11:33:16 -0300

    configurando animações com react spring

--- gobarber-web/src/components/ToastContainer/index.tsx
+++ gobarber-web/src/components/ToastContainer/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
 
 import { ToastMessage } from '../../hooks/toast';
 import Toast from './Toast';
@@ -10,10 +11,35 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransitions = useTransition(
+    messages,
+    message => message.id,
+    {
+      from: {
+        right: '-120%',
+        opacity: 0,
+        transform: 'rotateX(0deg)',
+        transition: 'transform 0.15s',
+      },
+      enter: {
+        right: '0%',
+        opacity: 1,
+        transform: 'rotateX(360deg)',
+        transition: 'transform 0.15s',
+      },
+      leave: {
+        right: '-120%',
+        opacity: 0,
+        transform: 'rotateX(0deg)',
+        transition: 'transform 0.15s',
+      },
+    },
+  );
+
   return (
     <Container>
-      {messages.map(message => (
-        <Toast key={message.id} message={message}></Toast>
+      {messagesWithTransitions.map(({ item, key, props }) => (
+        <Toast key={key} style={props} message={item}></Toast>
       ))}
     </Container>
   );
