e001cb5ad93bac16131a21aa5a48c35245b773a2
Guilherme Peixoto <guilherme@agily.com.br>
2020-05-11 15:55:16 -0300

    inicializacao das configuracoes das rotas

---
+++ gobarber-app/src/pages/SignUp/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Text>SignIn!</Text>
+    </Container>
+  );
+};
+
+export default SignUp;
