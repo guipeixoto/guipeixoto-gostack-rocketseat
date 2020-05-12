e66ec55b968d29d8024946d1d456c4653ec60bd6
Guilherme Peixoto <guilherme@agily.com.br>
2020-05-12 11:52:20 -0300

    tela de signup e roteamento

--- gobarber-app/src/pages/SignIn/styles.ts
+++ gobarber-app/src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const KeyboardAvoidingView = styled.KeyboardAvoidingView`
@@ -11,7 +12,7 @@ export const KeyboardAvoidingView = styled.KeyboardAvoidingView`
 export const Container = styled.View`
   align-items: center;
   justify-content: center;
-  padding: 0 30px;
+  padding: 0 30px ${Platform.OS === 'android' ? 70 : 40}px;
 `;
 
 export const Title = styled.Text`
--- gobarber-app/src/pages/SignUp/index.tsx
+++ gobarber-app/src/pages/SignUp/index.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, Platform, View, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {
+  Container,
+  Title,
+  BackToSignIn,
+  BackToSignInText,
+  KeyboardAvoidingView,
+} from './styles';
 
 const SignUp: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
-    <Container>
-      <Text>SignIn!</Text>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView keyboardShouldPersistTaps='handled'>
+          <Container>
+            <Image source={logoImg}></Image>
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name='name' icon='user' placeholder='Nome' />
+            <Input name='email' icon='mail' placeholder='E-mail' />
+            <Input name='password' icon='lock' placeholder='Senha' />
+
+            <Button onPress={() => console.log('')}>Cadastrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn onPress={() => navigation.goBack()}>
+        <Icon name='arrow-left' size={20} color='#fff' />
+        <BackToSignInText>Voltar para logon</BackToSignInText>
+      </BackToSignIn>
+    </>
   );
 };
 
