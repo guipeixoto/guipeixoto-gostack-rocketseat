c23e06e985f5cc00bad7d4bcdfd5d5ee02b04431
Guilherme Peixoto <guilherme@agily.com.br>
2020-05-12 16:48:46 -0300

    integrando unform do rocketseat

--- gobarber-app/src/pages/SignIn/index.tsx
+++ gobarber-app/src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Image, Platform, View, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import logoImg from '../../assets/logo.png';
 import Input from '../../components/Input';
@@ -18,7 +20,12 @@ import {
 } from './styles';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
+
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data);
+  }, []);
 
   return (
     <>
@@ -33,10 +40,14 @@ const SignIn: React.FC = () => {
               <Title>Faça seu logon</Title>
             </View>
 
-            <Input name='email' icon='mail' placeholder='E-mail' />
-            <Input name='password' icon='lock' placeholder='Senha' />
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input name='email' icon='mail' placeholder='E-mail' />
+              <Input name='password' icon='lock' placeholder='Senha' />
 
-            <Button onPress={() => console.log('')}>Entrar</Button>
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Entrar
+              </Button>
+            </Form>
 
             <ForgotPassword onPress={() => {}}>
               <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
--- gobarber-app/src/pages/SignUp/index.tsx
+++ gobarber-app/src/pages/SignUp/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Image, Platform, View, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import logoImg from '../../assets/logo.png';
 import Input from '../../components/Input';
@@ -16,7 +18,12 @@ import {
 } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
+
+  const handleSignUp = useCallback((data: object) => {
+    console.log(data);
+  }, []);
 
   return (
     <>
@@ -31,11 +38,15 @@ const SignUp: React.FC = () => {
               <Title>Crie sua conta</Title>
             </View>
 
-            <Input name='name' icon='user' placeholder='Nome' />
-            <Input name='email' icon='mail' placeholder='E-mail' />
-            <Input name='password' icon='lock' placeholder='Senha' />
+            <Form ref={formRef} onSubmit={handleSignUp}>
+              <Input name='name' icon='user' placeholder='Nome' />
+              <Input name='email' icon='mail' placeholder='E-mail' />
+              <Input name='password' icon='lock' placeholder='Senha' />
+            </Form>
 
-            <Button onPress={() => console.log('')}>Cadastrar</Button>
+            <Button onPress={() => formRef.current?.submitForm()}>
+              Cadastrar
+            </Button>
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
