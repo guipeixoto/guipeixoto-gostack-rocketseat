f3416130cc11109c7391873016c85992b5bc622a
Guilherme Peixoto <guilherme@agily.com.br>
2020-05-12 10:26:27 -0300

    tela de autenticação com scroll, efeito,

--- gobarber-app/src/pages/SignIn/index.tsx
+++ gobarber-app/src/pages/SignIn/index.tsx
@@ -1,23 +1,52 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, Platform, View, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/logo.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+  KeyboardAvoidingView,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg}></Image>
-      <Title>Faça seu logon</Title>
+    <>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView keyboardShouldPersistTaps='handled'>
+          <Container>
+            <Image source={logoImg}></Image>
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-      <Input name='email' icon='mail' placeholder='E-mail' />
-      <Input name='email' icon='lock' placeholder='Senha' />
+            <Input name='email' icon='mail' placeholder='E-mail' />
+            <Input name='password' icon='lock' placeholder='Senha' />
 
-      <Button onPress={() => console.log('')}>Entrar</Button>
-    </Container>
+            <Button onPress={() => console.log('')}>Entrar</Button>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton>
+        <Icon name='log-in' size={20} color='#ff9000' />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
