1a110f50d5ab7cfb3d9cd1a9402bba622d0b7e27
Guilherme Peixoto <guilherme@agily.com.br>
2020-05-14 12:42:18 -0300

    aplicando DDD / alteracao de estrutura / organizacao de responsabilidades

--- gobarber-backend/src/shared/infra/http/server.ts
+++ gobarber-backend/src/shared/infra/http/server.ts
@@ -3,12 +3,12 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
-import routes from './routes';
 
-import AppError from './errors/AppError';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import routes from '@shared/infra/http/routes';
 
-import './database';
-import uploadConfig from './config/upload';
+import '@shared/infra/typeorm';
 
 const app = express();
 app.use(cors());
