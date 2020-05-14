eb88b637e9a91af09fe38457fbb8da2dea1547d7
Guilherme Peixoto <guilherme@agily.com.br>
2020-05-14 14:20:54 -0300

    reescrevendo repositorios / desacoplando dependencias

--- gobarber-backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ gobarber-backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,8 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 
+import ICreateAppoitmentDTO from '../dtos/ICreateAppoitmentDTO';
+
 export default interface IAppointmentsRepository {
+  create(data: ICreateAppoitmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
--- gobarber-backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ gobarber-backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -2,16 +2,17 @@ import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
 import AppError from '@shared/errors/AppError';
+
+import ICreateAppoitmentDTO from '../dtos/ICreateAppoitmentDTO';
+
 import Appointment from '../infra/typeorm/entities/Appointment';
 import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRepository';
 
-interface RequestDTO {
-  provider_id: string;
-  date: Date;
-}
-
 class CreateAppointmentService {
-  public async run({ provider_id, date }: RequestDTO): Promise<Appointment> {
+  public async run({
+    provider_id,
+    date,
+  }: ICreateAppoitmentDTO): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(date);
@@ -24,12 +25,10 @@ class CreateAppointmentService {
       throw new AppError('This appointment is already booked');
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
-
-    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
