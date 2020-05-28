4a9f70f0aab8cf9b6293295c29c9363f9dc678a8
Guilherme Peixoto <guilherme@agily.com.br>
2020-05-28 17:22:04 -0300

    criacao do mail provider

--- gobarber-backend/src/shared/container/providers/index.ts
+++ gobarber-backend/src/shared/container/providers/index.ts
@@ -3,7 +3,14 @@ import { container } from 'tsyringe';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 
+import IMailProvider from './MailProvider/models/IMailProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
+
+// container.registerSingleton<IMailProvider>(
+//   'MailProvider',
+//   aaaaaa
+)
