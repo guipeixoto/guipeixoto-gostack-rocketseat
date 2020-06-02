267adc40a6cdff5cdb60588a92a3e5cdb5433f99
Guilherme Peixoto <guilherme@agily.com.br>
2020-06-02 15:27:05 -0300

    configuracao de template para envio de emails

--- gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -31,9 +31,19 @@ export default class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
-    await this.mailProvider.sendMail(
-      email,
-      `Pedido de recuperaçã de senha: ${token}`,
-    );
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        template: 'Olá, <b>{{name}}</b>: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
