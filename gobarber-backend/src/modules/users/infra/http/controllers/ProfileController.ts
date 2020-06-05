a66836f3b582ec23d191ed6cff2f881e364921d1
Guilherme Peixoto <guilherme@agily.com.br>
2020-06-05 10:15:56 -0300

    exibicao do profile do usuário/teste

--- gobarber-backend/src/modules/users/infra/http/controllers/ProfileController.ts
+++ gobarber-backend/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -2,13 +2,18 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+import ShowProfileService from '@modules/users/services/ShowProfileService';
 
 export default class ProfileController {
-  // public async show(request: Request, response: Response): Promise<User> {
-  //   const id = request.user.id;
+  public async show(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+    const showProfileService = container.resolve(ShowProfileService);
+    const user = await showProfileService.run({ user_id });
 
-  //   return
-  // }
+    delete user.password;
+
+    return response.json(user);
+  }
 
   public async update(request: Request, response: Response): Promise<Response> {
     const { name, email, old_password, password } = request.body;
