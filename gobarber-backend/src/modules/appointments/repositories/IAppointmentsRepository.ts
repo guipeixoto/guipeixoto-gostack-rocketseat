2a30becb9f20fb96a11888c4f4da4495e2b40bf0
Guilherme Peixoto <guilherme@agily.com.br>
2020-06-05 15:37:09 -0300

    listagem de dias com horarios disponiveis baseado no mes e ano

--- gobarber-backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ gobarber-backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,8 +1,12 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 import ICreateAppoitmentDTO from '../dtos/ICreateAppoitmentDTO';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppoitmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
 }
