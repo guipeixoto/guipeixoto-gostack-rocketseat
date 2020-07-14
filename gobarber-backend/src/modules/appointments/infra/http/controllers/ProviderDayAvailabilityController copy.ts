9a6b777357b5a76aedf2d9565823f25ab1037677
Guilherme Peixoto <guilherme@agily.com.br>
2020-07-14 00:14:50 -0300

    horarios do prestador disponiveis por mes e por dia

---
+++ gobarber-backend/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController copy.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+export default class ProviderDayAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { day, month, year } = request.body;
+    const { provider_id } = request.params;
+
+    const listProviderDayAvailabilityService = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+    const availability = await listProviderDayAvailabilityService.run({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return response.json(availability);
+  }
+}
