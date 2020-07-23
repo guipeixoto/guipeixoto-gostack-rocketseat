7b34aaae9e0de5d97bf80121dbc0abc7c5d02036
Guilherme Peixoto <guilherme@agily.com.br>
2020-07-23 18:38:46 -0300

    ajustes backend

--- gobarber-backend/src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ gobarber-backend/src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 
@@ -7,8 +8,9 @@ export default class ProvidersController {
   public async index(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
     const listProvidersService = container.resolve(ListProvidersService);
+
     const providers = await listProvidersService.run({ user_id });
 
-    return response.json(providers);
+    return response.json(classToClass(providers));
   }
 }
