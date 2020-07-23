b1d33694c7249492631e1738f4f6d4b347208ab1
Guilherme Peixoto <guilherme@agily.com.br>
2020-07-23 12:02:45 -0300

    passando params para query

--- gobarber-backend/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController copy.ts
+++ gobarber-backend/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController copy.ts
@@ -5,17 +5,17 @@ import ListProviderDayAvailabilityService from '@modules/appointments/services/L
 
 export default class ProviderDayAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const { day, month, year } = request.body;
     const { provider_id } = request.params;
+    const { day, month, year } = request.query;
 
     const listProviderDayAvailabilityService = container.resolve(
       ListProviderDayAvailabilityService,
     );
     const availability = await listProviderDayAvailabilityService.run({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(availability);
--- gobarber-backend/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ gobarber-backend/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -5,16 +5,16 @@ import ListProviderMonthAvailabilityService from '@modules/appointments/services
 
 export default class ProviderMonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const { month, year } = request.body;
     const { provider_id } = request.params;
+    const { month, year } = request.query;
 
     const listProviderMonthAvailabilityService = container.resolve(
       ListProviderMonthAvailabilityService,
     );
     const availability = await listProviderMonthAvailabilityService.run({
       provider_id,
-      month,
-      year,
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(availability);
