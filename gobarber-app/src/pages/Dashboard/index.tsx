3da8c8942e602e216bb7495be63c2e5d4ccbda6a
Guilherme Peixoto <guilherme@agily.com.br>
2020-08-04 11:08:49 -0300

    ajustes

--- gobarber-app/src/pages/Dashboard/index.tsx
+++ gobarber-app/src/pages/Dashboard/index.tsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+  ProvidersList,
+} from './styles';
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  const { signOut, user } = useAuth();
+  const { navigate } = useNavigation();
+
+  useEffect(() => {
+    api.get('/providers').then(response => setProviders(response.data));
+  }, []);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', paddingHorizontal: 16 }}>
-      <Button title='Logout' onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={() => navigate('Profile')}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        renderItem={({ item }) => <UserName>{item.name}</UserName>}
+      />
+
+      <View
+        style={{ flex: 1, justifyContent: 'center', paddingHorizontal: 16 }}
+      >
+        <Button title='Logout' onPress={signOut} />
+      </View>
+    </Container>
   );
 };
 
